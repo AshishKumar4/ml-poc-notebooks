8806c0644949ebe530222ae2845af191a5578549
Ashish Kumar Singh <ashishkmr472@gmail.com>
2024-07-06 02:38:22 +0530

    feat: latest

---
+++ diffusion experiments/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+required_packages=[
+    'flax==0.8.4', 
+    'optax>=0.2.2', 
+    'jax==0.4.28',
+]
+
+setup(
+    name='FlaxDiff',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A versatile and easy to understand Diffusion library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ashish Kumar Singh',
+    author_email='ashishkmr472@gmail.com',
+    install_requires=required_packages,
+)
